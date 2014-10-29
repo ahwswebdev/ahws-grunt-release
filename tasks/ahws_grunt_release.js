a13b70e56f7fb1b4cef3649c3e634c72bcc8a380
xander <xander.bosch@ahold.com>
2014-10-29 08:48:17 +0100

    added option to pass in bower.json file path and name instead of dependency object, because this may change during a bower task and then it's pointing to the old object, so always read the latest version from disk

--- tasks/ahws_grunt_release.js
+++ tasks/ahws_grunt_release.js
@@ -1,3 +1,8 @@
+/*globals require, module*/
+var exec = require('child_process').exec,
+    replace = require("replace"),
+    lodash = require("lodash");
+
 /*
  * ahws-grunt-release
  * https://github.com/ahwswebdev/ahws-grunt-release
@@ -5,89 +10,93 @@
  * Copyright (c) 2014 ahwswebdev
  * Licensed under the MIT license.
  */
-
-'use strict';
-
-var exec = require('child_process').exec;
-var replace = require("replace");
-var _ = require("lodash");
-
 module.exports = function (grunt) {
+
+    'use strict';
 
     grunt.registerMultiTask('ahws_grunt_release', 'Update bower dependencies to the latest tags in git repo', function () {
 
-        var getSize = function (dependencies) {
-            var key, count = 0;
-            for (key in dependencies) {
-                if (dependencies.hasOwnProperty(key)) {
-                    count++;
-                }
-            }
-            return count;
-        };
-
-        var replaceVersionNumber = function (dependency, currentVersion, releaseVersion) {
-            if (!releaseVersion) {
-                grunt.log.errorlns('No version number found for: ' + dependency);
-            }
-
-            grunt.log.writeln('Replace version number for ' + dependency + currentVersion + ' with ' + dependency + "#" + releaseVersion);
-            replace({
-                regex: dependency + currentVersion,
-                replacement: dependency + "#" + releaseVersion,
-                paths: ['bower.json'],
-                recursive: false,
-                silent: false
-            });
-
-            after();
-        };
-
-        var findLatestVersionNumber = function (dependency, currentVersion, stdout) {
-            grunt.log.writeln('Find latest version number for ' + dependency);
-            var lines = stdout.toString().split('\n'),
-                latestVersionNumber = false;
-
-            lines.forEach(function (line) {
-                var parts = line.split('/v');
-                if (parts[1]) {
-                    latestVersionNumber = parts[1].replace('^{}', '')
-                }
-            });
-
-            replaceVersionNumber(dependency, currentVersion, latestVersionNumber);
-        };
-
-        var getVersion = function (dependency, currentVersion, gitRepositoryUrl) {
-            exec('git ls-remote --tags ' + gitRepositoryUrl, function (err, stdout, stderr) {
-                if (err) {
-                    grunt.warn(err);
-                } else {
-                    findLatestVersionNumber(dependency, currentVersion, stdout);
-                }
-            });
-        };
-
         var options = this.options({
-            branch: false
-        });
+                dependenciesFileName: 'bower.json',
+                branch: false
+            }),
 
-        var done = this.async(),
-            after = _.after(getSize(this.data.dependencies), done);
+            getSize = function (dependencies) {
+                var key, count = 0;
+                for (key in dependencies) {
+                    if (dependencies.hasOwnProperty(key)) {
+                        count = count + 1;
+                    }
+                }
+                return count;
+            },
 
-        for (var dependency in this.data.dependencies) {
-            var gitRepositoryUrl = this.data.dependencies[dependency].replace('git+https', 'https').replace(/#[a-z0-9.]+/, ''),
-                currentVersion = this.data.dependencies[dependency].match(/#[a-z0-9.]+/)[0];
+            dependency,
+            bowerConfig = grunt.file.readJSON(options.dependenciesFileName),
+            done = this.async(),
+            after = lodash.after(getSize(bowerConfig.dependencies), done),
 
-            if (options.branch) {
-                grunt.log.writeln('Setting version for: ' + gitRepositoryUrl + ' from version: ' + currentVersion + ' to branch: ' + options.branch);
-                replaceVersionNumber(dependency, currentVersion, options.branch)
-            } else {
-                grunt.log.writeln('Getting version for: ' + gitRepositoryUrl + ' with version: ' + currentVersion);
-                getVersion(dependency, currentVersion, gitRepositoryUrl)
+            replaceVersionNumber = function (dependency, currentVersion, releaseVersion) {
+                if (!releaseVersion) {
+                    grunt.log.errorlns('No version number found for: ' + dependency);
+                }
+
+                grunt.log.writeln('Replace version number for ' + dependency + currentVersion + ' with ' + dependency + "#" + releaseVersion);
+                replace({
+                    regex: dependency + currentVersion,
+                    replacement: dependency + "#" + releaseVersion,
+                    paths: ['bower.json'],
+                    recursive: false,
+                    silent: false
+                });
+
+                after();
+            },
+
+            findLatestVersionNumber = function (dependency, currentVersion, stdout) {
+                grunt.log.writeln('Find latest version number for ' + dependency);
+                var lines = stdout.toString().split('\n'),
+                    latestVersionNumber = false;
+
+                lines.forEach(function (line) {
+                    var parts = line.split('/v');
+                    if (parts[1]) {
+                        latestVersionNumber = parts[1].replace('^{}', '');
+                    }
+                });
+
+                replaceVersionNumber(dependency, currentVersion, latestVersionNumber);
+            },
+
+            getVersion = function (dependency, currentVersion, gitRepositoryUrl) {
+                exec('git ls-remote --tags ' + gitRepositoryUrl, function (err, stdout) {
+                    if (err) {
+                        grunt.warn(err);
+                    } else {
+                        findLatestVersionNumber(dependency, currentVersion, stdout);
+                    }
+                });
+            },
+
+            determineTask = function (dependency) {
+                var gitRepositoryUrl = dependency.replace('git+https', 'https').replace(/#[a-z0-9.]+/, ''),
+                    currentVersion = dependency.match(/#[a-z0-9.]+/)[0];
+
+                if (options.branch) {
+                    grunt.log.writeln('Setting version for: ' + gitRepositoryUrl + ' from version: ' + currentVersion + ' to branch: ' + options.branch);
+                    replaceVersionNumber(dependency, currentVersion, options.branch);
+                } else {
+                    grunt.log.writeln('Getting version for: ' + gitRepositoryUrl + ' with version: ' + currentVersion);
+                    getVersion(dependency, currentVersion, gitRepositoryUrl);
+                }
+            };
+
+        grunt.log.writeln('Options > dependenciesFileName: ' + options.dependenciesFileName + ' branch: ' + options.branch);
+
+        for (dependency in bowerConfig.dependencies) {
+            if (bowerConfig.dependencies.hasOwnProperty(dependency)) {
+                determineTask(bowerConfig.dependencies[dependency]);
             }
         }
     });
 };
-
-
