57d92f6d7cc3396705bd8cd9c31925c3dd831087
Rick Ekelschot <rick@frontmen.nl>
2015-10-14 13:48:34 +0200

    Use node-semver for determining highest version number

--- tasks/utils/latest-version.js
+++ tasks/utils/latest-version.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var exec = require('child_process').exec,
-    Promise = require('promise');
+    Promise = require('promise'),
+    semver = require('semver');
 
 module.exports = function (grunt) {
 
@@ -9,15 +10,20 @@ module.exports = function (grunt) {
             grunt.log.writeln('Find latest version number for ' + gitRepositoryUrl);
 
             var lines = stdout.toString().replace(/v/g, '')
-                .split('\n');
+                .split('\n'),
+                latestVersion;
 
-            return lines.sort(function (a, b) {
-                if (versionNumberHigher(a, b)) {
-                    return 1;
+            lines.forEach(function (version) {
+                if (semver.valid(version)) {
+                    if (!latestVersion) {
+                        latestVersion = version;
+                    } else if(semver.gt(version, latestVersion)) {
+                        latestVersion = version;
+                    }
                 }
+            });
 
-                return -1;
-            })[0];
+            return latestVersion;
         },
 
         getVersion = function (gitRepositoryUrl) {
@@ -31,40 +37,13 @@ module.exports = function (grunt) {
                     }
                 });
             });
-        },
-
-        getVersionArray = function (version) {
-            return version.split('.')
-                .map(function (val) {
-                    return parseInt(val, 10);
-                });
-        },
-
-        versionNumberHigher = function(versionA, versionB) {
-            if (!versionA) {
-                return true;
-            }
-
-            versionA = getVersionArray(versionA);
-            versionB = getVersionArray(versionB);
-
-            if (versionB[0] > versionA[0]) {
-                return true;
-            }
-
-            if (versionB[0] >= versionA[0] && versionB[1] > versionA[1]) {
-                return true;
-            }
-
-            if (versionB[0] >= versionA[0] && versionB[1] >= versionA[1] && versionB[2] > versionA[2]) {
-                return true;
-            }
         };
 
     return {
         getLatestVersionNumber: function (gitRepositoryUrl) {
             return new Promise(function (resolve) {
                 getVersion(gitRepositoryUrl).then(function (latestVersion) {
+                    grunt.log.writeln('version = ' + latestVersion);
                     resolve(latestVersion);
                 });
             });
